3f99a7fe09ee368e8b13e21a1dbd4579be0c249b
Jaime Duque <jaime.duque@estratek.com.co>
2020-02-14 00:19:59 -0500

    The user is inserted in the DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
-exports.createUser = (req, res) => {
-    console.log(req.body);
+const User = require('../models/User');
+
+exports.createUser = async (req, res) => {
+    try {
+       let user;
+       
+       // Create the new user
+       user = new User(req.body);
+
+       // save user
+       await user.save();
+
+        // Confirmation message
+        res.send('User created successfully');
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('There was an error');
+    }
 }
