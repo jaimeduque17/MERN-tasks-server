19739ea1383c6caa67582e58283ff08c945cd67c
Jaime Duque <jaime.duque@estratek.com.co>
2020-02-13 22:45:13 -0500

    Users routes are configured

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ connectDB();
 // Server port
 const PORT = process.env.PORT || 4000;
 
+// Import routes
+app.use('/api/users', require('./routes/users'));
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`The server is running in the port ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+// Routes to create users
+const express = require('express');
+const router = express.Router();
+
+// Create an user
+// api/users
+router.post('/', () => {
+
+});
+
+module.exports = router;
