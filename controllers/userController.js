bcb803edba23b149314b2fa52d6d8d0b813b065c
Jaime Duque <jaime.duque@estratek.com.co>
2020-02-16 21:06:03 -0500

    Some validations are added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,38 @@
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 exports.createUser = async (req, res) => {
 
+    // Check if there are errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     // Extract e-mail and password
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
         // check that the registered user is unique
-       let user = await User.findOne({email});
+        let user = await User.findOne({ email });
 
-       if(user) {
-           return res.status(400).json({msg: 'The user is already exist'});
-       }
-       
-       // Create the new user
-       user = new User(req.body);
+        if (user) {
+            return res.status(400).json({ msg: 'The user is already exist' });
+        }
 
-       // Encrypt password
-       const salt = await bcryptjs.genSalt(10);
-       user.password = await bcryptjs.hash(password, salt);
+        // Create the new user
+        user = new User(req.body);
 
-       // save user
-       await user.save();
+        // Encrypt password
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(password, salt);
+
+        // save user
+        await user.save();
 
         // Confirmation message
-        res.json({msg: 'User created successfully'});
+        res.json({ msg: 'User created successfully' });
 
     } catch (error) {
         console.log(error);
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { check } = require('express-validator');
 
 // Create an user
 // api/users
 router.post('/',
+    [
+        check('name', 'The name is required').not().isEmpty(),
+        check('email', 'Add a valid email').isEmail(),
+        check('password', 'The password must be at least 6 characters').isLength({ min: 6 })
+    ],
     userController.createUser
 );
 
