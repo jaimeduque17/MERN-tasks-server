69105383f184eed8a7055e428ea20622cd31a7ae
Jaime Duque <jaime.duque@estratek.com.co>
2020-02-16 20:45:39 -0500

    Encrypt password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,31 @@
 const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
 
 exports.createUser = async (req, res) => {
+
+    // Extract e-mail and password
+    const {email, password} = req.body;
+
     try {
-       let user;
+        // check that the registered user is unique
+       let user = await User.findOne({email});
+
+       if(user) {
+           return res.status(400).json({msg: 'The user is already exist'});
+       }
        
        // Create the new user
        user = new User(req.body);
+
+       // Encrypt password
+       const salt = await bcryptjs.genSalt(10);
+       user.password = await bcryptjs.hash(password, salt);
 
        // save user
        await user.save();
 
         // Confirmation message
-        res.send('User created successfully');
+        res.json({msg: 'User created successfully'});
 
     } catch (error) {
         console.log(error);
