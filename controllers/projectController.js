3e4c8d156bf088d8ab434eb37efc674db6604a3f
Jaime Duque <jaime.duque@estratek.com.co>
2020-02-25 23:22:44 -0500

    Functionality to add new projects is created

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,3 +1,14 @@
+const Project = require('../models/Project');
+
 exports.createProject = async (req, res) => {
-    
+    try {
+        // Create a new project
+        const project = new Project(req.body);
+        project.save();
+        res.json(project);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('There was an error');        
+    }
 }
